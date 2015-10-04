df0de623119b87fe7d1f7b3dcc3e22fe2d409a4e
GogoK <georgiakanchev@gmail.com>
2015-10-04 21:43:28 +0300

    exam-2

---
+++ 14.PetarGame/PetarGame.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class PetarGame
+{
+    static void Main()
+    {
+        ulong start = ulong.Parse(Console.ReadLine());
+        ulong end = ulong.Parse(Console.ReadLine());
+        string replacement = Console.ReadLine();
+
+        decimal sum = 0;
+        for (ulong i = start; i < end; i++)
+        {
+            if (i % 5 == 0)
+            {
+                sum += i;
+            }
+            else
+            {
+                sum += i % 5;
+            }
+        }
+
+        string output = sum.ToString();
+        if (sum % 2 == 1)
+        {
+            string last = (sum % 10).ToString();
+            output = output.Replace(last, replacement);
+            Console.WriteLine(output);
+        }
+        else
+        {
+            string first = output[0].ToString();
+            output = output.Replace(first, replacement);
+            Console.WriteLine(output);
+        }
+    }
+}
