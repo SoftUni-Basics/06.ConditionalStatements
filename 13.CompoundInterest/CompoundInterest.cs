878e7e5502543bf3b0f99e6b48373142c41ca694
GogoK <georgiakanchev@gmail.com>
2015-10-04 21:30:17 +0300

    exam-1

---
+++ 13.CompoundInterest/CompoundInterest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class CompoundInterest
+{
+    static void Main()
+    {
+        double tvPrice = double.Parse(Console.ReadLine());
+        int n = int.Parse(Console.ReadLine());
+        double i = double.Parse(Console.ReadLine());
+        double f = double.Parse(Console.ReadLine());
+
+        double bank = tvPrice * Math.Pow(1 + i, n);
+        double friend = tvPrice * (1 + f);
+
+        if (bank < friend)
+        {
+            Console.WriteLine("{0:F} Bank", bank);
+        }
+        else
+        {
+            Console.WriteLine("{0:F} Friend", friend);
+        }
+    }
+}
